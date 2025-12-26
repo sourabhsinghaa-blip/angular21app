e454574eef61473ba0e98cbf649f57bbd8e02536
amit kaushik <amit@gmail.com>
2025-12-26 07:26:04 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee/employee';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Employee],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
